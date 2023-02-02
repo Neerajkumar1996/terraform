ad1359c7fe607a38d7c6f1b0e279fe7f68289fda
NEERAJ KUMAR <123727900+Neerajkumar1996@users.noreply.github.com>
2023-02-02 21:50:52 +0530

    Update main.tf

--- modules/pvtsg/main.tf
+++ modules/pvtsg/main.tf
@@ -2,48 +2,57 @@ resource "aws_security_group" "private_security_group" {
   name        = var.sg_pvt
   description = "security group"
   vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Traffic from VPC"
-    from_port   = var.https_port
-    to_port     = var.https_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
-  }
-  ingress {
-    description = "Traffic from VPC"
-    from_port   = var.ssh_port
-    to_port     = var.ssh_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
-  }
-   ingress {
-    description = "port for rd-database "
-    from_port   = var.rd_database_port
-    to_port     = var.rd_database_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
-  }
-  ingress {
-    description = "port for rd-rest-api "
-    from_port   = var.rd_restAPI_port
-    to_port     = var.rd_restAPI_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
-  }
-  ingress {
-    description = "port for rd-crossCluster-search "
-    from_port   = var.rd_crossCluster_port
-    to_port     = var.rd_crossCluster_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
-  }
-  ingress {
-    description = "port for rd-nodeCommunication "
-    from_port   = var.rd_nodeCommunication_port
-    to_port     = var.rd_nodeCommunication_port
-    protocol    = "tcp"
-    cidr_blocks = [var.cidr]
+  
+  dynamic "ingress" {
+    for_each = local.inbound_ports
+    content {
+       from_port        = ingress.value
+     to_port          = ingress.value
+     protocol         = "tcp"
+     cidr_blocks       = var.sg_cidr_block
+   }
+ }
+#   ingress {
+#     description = "Traffic from VPC"
+#     from_port   = var.https_port
+#     to_port     = var.https_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#   }
+#   ingress {
+#     description = "Traffic from VPC"
+#     from_port   = var.ssh_port
+#     to_port     = var.ssh_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#   }
+#    ingress {
+#     description = "port for rd-database "
+#     from_port   = var.rd_database_port
+#     to_port     = var.rd_database_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#   }
+#   ingress {
+#     description = "port for rd-rest-api "
+#     from_port   = var.rd_restAPI_port
+#     to_port     = var.rd_restAPI_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#   }
+#   ingress {
+#     description = "port for rd-crossCluster-search "
+#     from_port   = var.rd_crossCluster_port
+#     to_port     = var.rd_crossCluster_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#   }
+#   ingress {
+#     description = "port for rd-nodeCommunication "
+#     from_port   = var.rd_nodeCommunication_port
+#     to_port     = var.rd_nodeCommunication_port
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
   }
   # ingress {
   #   description = "port for Performance Analyzer "
@@ -63,3 +72,7 @@ resource "aws_security_group" "private_security_group" {
 
    tags = var.pvt_sg_tags
  }
+
+locals {
+ inbound_ports = [8443, 22, 10000, 9443,7000, 3333 ,8002 ]
+}
